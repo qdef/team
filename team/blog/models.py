da745bbfa97b0c049836941a4d04ff5ece4f5853
qdef <quentin.defenouillere@gmail.com>
2018-01-23 12:55:26 +0100

    added first blog articles

--- team/blog/models.py
+++ team/blog/models.py
@@ -1,12 +1,21 @@
 from django.db import models
-from django.core.urlresolvers import reverse
+#from django.core.urlresolvers import reverse
 
-class Post(models.Model):
+class BlogArticles(models.Model):
+	FOOTBALL = 'FB'
+	RUGBY = 'RB'
+	AMERICAN = 'AF'
+	SPORTS_CHOICES = (
+	    (FOOTBALL, 'Football'),
+            (RUGBY, 'Rugby'),
+            (AMERICAN, 'American Football'),
+	    )
 	title = models.CharField(max_length=200)
-	sport = models.CharField(   )
+	sport = models.CharField(choices=SPORTS_CHOICES, max_length=10)
 	body = models.TextField()
 	image = models.FileField(null=True, blank=True)
-	date = models.DateTimeField()
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
 	
 	def __str__(self):
 		return self.title

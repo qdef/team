b121116834b8b0fac1deb6628090592391c39c9d
qdef <quentin.defenouillere@gmail.com>
2018-01-23 12:13:51 +0100

    added hyperlinks in blog page

--- team/blog/models.py
+++ team/blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	sport = models.CharField(   )
+	body = models.TextField()
+	image = models.FileField(null=True, blank=True)
+	date = models.DateTimeField()
+	
+	def __str__(self):
+		return self.title
+	
+#	def get_absolute_url(self):
+#		return reverse("detail", kwargs={"id":self.id})
+#		#return "/blog/%s/" %(self.id)
+	
+
